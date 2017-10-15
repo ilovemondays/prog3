609f5bd0527500e4ff8f21d5a5e98bc74c30d849
MariusLuding <mluding@gmail.com>
2017-10-15 12:15:00 -0400

    Just messing about

---
+++ src/Hello World.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Hello.c
+ Author      : MLuding
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Herro? Anyonz hiar?"); /* prints Herro? Anyonz hiar? */
+	return EXIT_SUCCESS;
+}
